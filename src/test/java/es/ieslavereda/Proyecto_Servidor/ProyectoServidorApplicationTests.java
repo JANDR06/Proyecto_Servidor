d40fbc55c288bf898e3d43181401a2fb0b451788
ajorgep <alejorpas2@alu.edu.gva.es>
2023-05-22 20:15:57 +0200

    Initial commit

---
+++ src/test/java/es/ieslavereda/Proyecto_Servidor/ProyectoServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.Proyecto_Servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
